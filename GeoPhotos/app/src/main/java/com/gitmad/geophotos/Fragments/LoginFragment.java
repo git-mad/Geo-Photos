f07df2efc98149d3c3c38d6baec6da443378b18f
brainmurphy1 <brianmurphy@gatech.edu>
2015-01-27 15:06:27 -0500

    Wrote LoginFragment with callback that passes a User Object to represent a signed in user or a LoginFailureReason enum. Transferred layout XML from login_layout.xml to fragment_login.xml

--- GeoPhotos/app/src/main/java/com/gitmad/geophotos/Fragments/LoginFragment.java
+++ GeoPhotos/app/src/main/java/com/gitmad/geophotos/Fragments/LoginFragment.java
@@ -1,52 +1,35 @@
 package com.gitmad.geophotos.Fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.gitmad.geophotos.Activities.HomeScreen;
+import com.gitmad.geophotos.Models.User;
 import com.gitmad.geophotos.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * {@link com.gitmad.geophotos.Fragments.LoginFragment.LoginListener} interface
  * to handle interaction events.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private LoginListener listener;
 
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    //Text field references//
+    private EditText emailEditText;
+    private EditText userNameEditText;
+    private EditText passwordEditText;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -55,31 +38,39 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        //Tell Fragment to save it's instance state//
+        setRetainInstance(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
-    }
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        //hold references for when user presses loginButton later//
+        emailEditText = (EditText) rootView.findViewById(R.id.emailEditText);
+        userNameEditText = (EditText) rootView.findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) rootView.findViewById(R.id.passwordEditText);
+
+        //set up onClickListener//
+        Button loginButton = (Button) rootView.findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        return rootView;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (OnFragmentInteractionListener) activity;
+            listener = (LoginListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -87,24 +78,95 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        if(emailEditText.getText() != null){
+            savedInstanceState.putString("emailAddress", emailEditText.getText().toString());
+        }
+        if(userNameEditText.getText()!= null){
+            savedInstanceState.putString("userName", userNameEditText.getText().toString());
+        }
+        if(passwordEditText.getText() != null){
+            savedInstanceState.putString("password", passwordEditText.getText().toString());
+        }
+
+        // Always call the superclass so it can save the view hierarchy state
+        //Notice how we do this last
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+//    @Override
+//    public void onViewStateRestored(Bundle savedInstanceState) {
+//        // Always call the superclass so it can restore the view hierarchy
+//        //Notice how we do this first
+//        super.onViewStateRestored(savedInstanceState);
+//
+//        if (savedInstanceState != null) {
+//            if (savedInstanceState.containsKey("emailAddress")) {
+//                emailEditText.setText(savedInstanceState.getString("emailAddress"));
+//            }
+//            if (savedInstanceState.containsKey("userName")) {
+//                userNameEditText.setText(savedInstanceState.getString("emailAddress"));
+//            }
+//            if (savedInstanceState.containsKey("password")) {
+//                passwordEditText.setText(savedInstanceState.getString("password"));
+//            }
+//        }
+//    }
+
+    @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        listener = null;
+    }
+
+    /*
+        Helper Methods
+     */
+
+    private void login() {
+        //TODO Authentication logic will be added here in the future.
+        //Hint: Use Extras with the intent to get send the username over.
+        //Pro Tip: Get the String from the EditText with userName.getText().toString();
+
+        Intent i = new Intent(getActivity(), HomeScreen.class);
+
+        if ((userNameEditText.getText() != null) && (passwordEditText.getText() != null)
+                    && (emailEditText.getText() != null)) {
+            i.putExtra("userName", userNameEditText.getText().toString());
+            i.putExtra("emailAddress", emailEditText.getText().toString());
+            i.putExtra("password", passwordEditText.getText().toString());
+        }
+
+        startActivity(i);
+    }
+
+
+    /*
+        Inner types
+     */
+
+    public interface LoginListener {
+        /**
+         * Called when the user was successfully authenticated
+         * @param user the non sensitive user info of the authenticated user
+         */
+        public void loginSuccessful(User user);
+
+        /**
+         * Called when the called when the user tries to log in, but cannot
+         * @param reason the reason that authentication failed, reptesented by the
+         *               {@link com.gitmad.geophotos.Fragments.LoginFragment.LoginFailureReason}
+         *               enum.
+         */
+        public void loginFailed(LoginFailureReason reason);
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     * enum that represents the reason that a login attempt failed.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    public enum LoginFailureReason {
+        USERNAME_NOT_FOUND,
+        PASSWORD_INCORRECT,
+        OTHER
     }
-
 }
---
+++ GeoPhotos/app/src/main/java/com/gitmad/geophotos/Models/User.java
@@ -0,0 +1,13 @@
+package com.gitmad.geophotos.Models;
+
+/**
+ * Class to encapsulate user info.
+ *
+ * Created by Brian on 1/27/2015.
+ */
+public class User {
+
+    private String userName;
+
+    //Other user info//
+}
